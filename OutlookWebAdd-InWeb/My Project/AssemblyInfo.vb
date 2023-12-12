1c6b9d901b1a6a9fd2a69466812cad83b65432f1
DioBytes <dyland601@gmail.com>
2023-12-12 12:23:48 -0500

    Add project files.

---
+++ OutlookWebAdd-InWeb/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("OutlookWebAdd_InWeb")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("University of Louisville")>
+<Assembly: AssemblyProduct("OutlookWebAdd_InWeb")>
+<Assembly: AssemblyCopyright("Copyright © University of Louisville 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("85e0230b-047e-4be9-8de9-1a14f408bab7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
